b9321db6d706dd3a414107fde41503cd6ee3a403
Asbjørn Mansa Jensen <asbamse@msn.com>
2016-03-08 23:51:29 +0100

    Dynamic Ini by inputs

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -8,14 +8,22 @@ public class HeapSort {
 	private static Scanner sc;
 
 	public static void main(String[] args) {
-		PQHeap Q = new PQHeap(5);
 		sc= new Scanner(System.in);
+		ArrayList<Element> tA = new ArrayList<>(); //Temporary input storage
+		int count = 0; //Counts inputs
 		
 		System.out.println("Please input 5 numbers to sort:");
 		
 		while (sc.hasNextInt()) {
-	    	Q.insert(new Element(sc.nextInt(), null));
+			tA.add(new Element(sc.nextInt(), null));
+			count++;
 		}
+		
+		PQHeap Q = new PQHeap(count); //Instantiates PQHeap with count entries
+		
+		for(int i=0; i<count; i++) {
+	    	Q.insert(tA.get(i)); // Inserts inputs 
+	    }
 		
         Q.printAll();
 	}
