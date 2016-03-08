8d6309670fe98fc5564428815808ba8e7dce3674
HackedByte <hackedbyte@gmail.com>
2016-03-08 13:43:36 +0100

    FIXED HeapSort

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -2,38 +2,36 @@ import java.util.*;
 
 public class HeapSort 
 {
-    public static void main(String[] args) 
+  
+	private static Scanner sc;
+
+	public static void main(String[] args) 
 	{
-        PQHeap Q = new PQHeap(5);
-		Scanner sc = new Scanner(System.in);
+		PQHeap Q = new PQHeap(5);
+		sc= new Scanner(System.in);
 		
 		System.out.println("Please input numbers to sort:");
 		
 		while (sc.hasNextInt()) {
 	    	Q.insert(new Element(sc.nextInt(), null));
 		}
-		//Q = MinHeapify(0, Q, 10);
-		//Q = BuildMinHeap(Q);
-        Q = Sort(Q);
+
+        Sort(Q);
 
 		System.out.println("Sorted:");
-		String str = " | ";
-		for (int k=0; k<Q.size(); k++)
+		while (Q.size() != 0)
 		{
-			str += Q.get(k)+ " | ";
+			System.out.println(Q.extractMin().key);
 		}
-		System.out.println(str);
 	}
 
 	public static PQHeap Sort(PQHeap Q) 
 	{
 		Q = BuildMinHeap(Q);
-		int l = Q.size();
-		for (int i=Q.size()-1; i>0; i--) 
+		for (int i = Q.size()+1; i < 0; i--) 
 		{
-			Q.replace(0,i);
-			l--;
-			Q.minHeapify(i, Q.size()-1);
+			PQHeap.replace(0,i);
+			PQHeap.minHeapify(i, Q.size()+1);
 		}
 		return Q;
 	}
@@ -42,7 +40,7 @@ public class HeapSort
 	{
 		for (int i=(int)Math.floor(Q.size()/2)-1; i>-1; i--)
 		{
-			Q.minHeapify(i, Q.size()-1);
+			PQHeap.minHeapify(i, Q.size()+1);
 		}
 		return Q;
 	}
